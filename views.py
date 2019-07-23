04902f51bd4cda29eb653539ad4617b430da6773
drw <drw@users.noreply.github.com>
2019-07-23 14:35:11 -0400

    Add prototype support for converting small data tables to Excel format

--- views.py
+++ views.py
@@ -1,8 +1,14 @@
 import requests, csv, ckanapi, time
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse, HttpResponse
 from django.shortcuts import redirect
 
-from .ckan_util import get_resource_parameter, get_number_of_rows
+from .ckan_util import get_resource_parameter, get_resource_name, get_package_title, get_number_of_rows
+
+from pprint import pprint
+try:
+    from icecream import ic
+except ImportError:  # Graceful fallback if IceCream isn't installed.
+    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
 
 DEFAULT_SITE = "https://data.wprdc.org"
 
@@ -23,6 +29,42 @@ def generate_header(ordered_fields, file_format):
         return ','.join(ordered_fields) + '\n'
     if file_format == 'tsv':
         return '\t'.join(ordered_fields) + '\n'
+
+def write_to_excel_format(ckan, resource_id, chunk_size):
+    records_format = 'objects'
+    r = ckan.action.datastore_search(id=resource_id, limit=chunk_size, offset=0, records_format=records_format) #, filters={field: search_term})
+    schema = eliminate_field(r['fields'],'_full_text') # Exclude _full_text from the schema.
+    ordered_fields = [f['id'] for f in schema]
+    data_rows = r['records']
+
+    from pytablewriter import ExcelXlsxTableWriter
+    writer = ExcelXlsxTableWriter()
+    package_name = get_package_title(DEFAULT_SITE, resource_id)
+    resource_name = get_resource_name(DEFAULT_SITE, resource_id)
+    writer.table_name = "{}|{}".format(package_name, resource_name) # Excel sheet names will not show these characters: /\*[]:?
+    # Also note that Excel sheet names get truncated to the first 31 characters.
+    writer.headers = ordered_fields
+    value_matrix = []
+    for row in data_rows:
+        new_row = [row[field] for field in ordered_fields]
+        value_matrix.append(new_row)
+    writer.value_matrix = value_matrix
+    #writer.value_matrix = [
+    #    [0,   0.1,      "hoge", True,   0,      "2017-01-01 03:04:05+0900"],
+    #    [2,   "-2.23",  "foo",  False,  None,   "2017-12-23 12:34:51+0900"],
+    #    [3,   0,        "bar",  "true",  "inf", "2017-03-03 22:44:55+0900"],
+    #    [-10, -9.9,     "",     "FALSE", "nan", "2017-01-01 00:00:00+0900"],
+    #]
+    local_filename = "{}.xlsx".format(resource_id)
+    writer.dump(local_filename)
+
+    with open(local_filename, 'rb') as fp:
+        data = fp.read()
+    #filename = 'some-filename.xlsx'
+    #response = HttpResponse(mimetype="application/ms-excel")
+    #response['Content-Disposition'] = 'attachment; filename=%s' % filename # force browser to download file
+    #response.write(data)
+    return data
 
 def get_and_write_next_rows(pseudo_buffer, ckan, resource_id, start_line=0, file_format='csv'):
     offset = start_line
@@ -67,12 +109,31 @@ def stream_response(request, resource_id, file_format='csv'):
 
     if file_format in ['csv', 'tsv']:
         content_type = 'text/{}'.format(file_format)
-    ckan = ckanapi.RemoteCKAN(DEFAULT_SITE)
-    response = StreamingHttpResponse(
-            streaming_content=(get_and_write_next_rows(Echo(), ckan, resource_id, 0, file_format)),
-        content_type=content_type,
-    )
-    # streaming_content: An iterator of strings representing the content.
+    if file_format in ['json']:
+        content_type = 'application/json'
+        raise ValueError("JSON is not yet supported.")
+    elif file_format in ['xlsx']:
+        content_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        #raise ValueError("Excel format is definitely not yet supported.")
+
+        excel_row_limit = 100000
+        row_count = get_number_of_rows(ckan, resource_id)
+        if row_count > excel_row_limit:
+            return HttpResponse("Excel files are not supported for row counts greater than {}.".format(excel_row_limit))
+        else:
+            response = HttpResponse(content_type=content_type)
+            filename = "{}.xlsx".format(resource_id)
+            response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+            response.write(write_to_excel_format(ckan, resource_id, excel_row_limit))
+            return response
+
+
+    else:
+        response = StreamingHttpResponse(
+                streaming_content=(get_and_write_next_rows(Echo(), ckan, resource_id, 0, file_format)),
+            content_type=content_type,
+        )
+        # streaming_content: An iterator of strings representing the content.
 
     response['Content-Disposition'] = 'attachment;filename={}.{}'.format(resource_id, file_format)
     return response
